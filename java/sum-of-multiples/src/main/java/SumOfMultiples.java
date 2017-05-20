2c1c721879369dfd48ab25a6a9801d56d599e9ae
roxanavp87 <roxanavp87@gmail.com>
2017-05-20 11:30:14 -0500

    -sum of multiples (other implementation using IntStream package)

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,5 +1,8 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class SumOfMultiples {
     private int[] set;
@@ -27,6 +30,39 @@ public class SumOfMultiples {
         }
 
         return sum;
+    }
+}
+
+
+class otherImplementation {
+    private final int sum;
+
+    public otherImplementation(int n, int[] ints) {
+        IntFunction<IntStream> multiplesOf =
+                i -> IntStream.iterate(i, mult -> mult + i).limit((n - 1) / i);
+        this.sum = IntStream.of(ints).flatMap(multiplesOf).distinct().sum();
+
+
+//        int i = 3;
+//        IntStream.iterate(5, mult -> mult + i).limit((n - 1) / i).forEach(System.out::println);
+//        int j = 5;
+//        IntStream.iterate(5, mult -> mult + j).limit((n - 1) / j).forEach(System.out::println);
+//        IntStream.of(ints).flatMap(multiplesOf).distinct().forEach(System.out::println);
+    }
+
+    public int getSum() {
+        return this.sum;
+    }
+}
+
+class test {
+    public static void main(String[] args) {
+        int[] set = {
+                3,
+                5
+        };
+        otherImplementation test = new otherImplementation(20, set);
+        System.out.println(test.getSum());
     }
 }
 
